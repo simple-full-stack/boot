0d6533e4c971b77dbbf513a3884f700531a12a65
yibuyisheng <yibuyisheng@163.com>
2017-09-30 01:13:14 +0800

    server

---
+++ packages/server/src/index.ts
@@ -0,0 +1,11 @@
+import * as express from 'express'
+
+const app = express();
+
+app.get('/', function (req, res) {
+    res.send('Hello World!');
+});
+
+app.listen(4000, function () {
+    console.log('Example app listening on port 4000!');
+});
